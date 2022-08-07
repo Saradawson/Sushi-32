8902cc6505ea0220d002de6ac823fe7031e784c4
unknown <saradawsone@gmail.com>
2022-08-07 00:52:15 -0700

    started home page

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sushi 32</h1>
+        <div className='navBar'>
+          <nav>
+            <a href=''>Menu</a>
+            <a href=''>About Us</a>
+            <a href=''>Order Online</a>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
